ced55c17b233e7b121b523d5862781bfdcef3458
Dylan Goble <dgoble0516@gmail.com>
2026-02-04 14:24:04 -0500

    feat: made top-films api

--- routes/landing_routes.py
+++ routes/landing_routes.py
@@ -1,16 +1,28 @@
 from flask import Blueprint, jsonify
+from db_config import get_db_connection;
 
 landing_bp = Blueprint('landing_bp', __name__)
 
 @landing_bp.route('/api/top-films', methods=['GET'])
 def get_top_films():
-    data = [
-        {"id": 1, "title": "ACADEMY DINOSAUR", "rentals": 34},
-        {"id": 2, "title": "ELEPHANT TROJAN", "rentals": 31},
-        {"id": 3, "title": "BUCKET BROTHERHOOD", "rentals": 30},
-        {"id": 4, "title": "RIVER OUTLAW", "rentals": 28},
-        {"id": 5, "title": "FORWARD TEMPLE", "rentals": 27}
-    ]
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
 
-    # 4. Serving the dish (Returning JSON)
-    return jsonify(data)
+    query = """
+        SELECT f.film_id, f.title, c.name AS category_name, COUNT(r.rental_id) AS rental_count
+        FROM film f
+        JOIN film_category fc ON f.film_id = fc.film_id
+        JOIN category c ON fc.category_id = c.category_id
+        JOIN inventory i ON f.film_id = i.film_id
+        JOIN rental r ON i.inventory_id = r.inventory_id
+        GROUP BY f.film_id, f.title, c.name
+        ORDER BY rental_count DESC
+        LIMIT 5;
+    """
+
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    return jsonify(results)
