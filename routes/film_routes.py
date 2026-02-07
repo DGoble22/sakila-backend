b33cea1cc036adadae93d0f6ec5d5ae862081426
Dylan Goble <dgoble0516@gmail.com>
2026-02-07 16:56:53 -0500

    feat: implementd film-details api

--- routes/film_routes.py
+++ routes/film_routes.py
@@ -1,2 +1,27 @@
 from flask import Blueprint, jsonify
+from db_config import get_db_connection;
 film_bp = Blueprint('film_bp', __name__)
+
+@film_bp.route('/api/film-details/<int:id>', methods=['GET'])
+def get_film_details(id):
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
+
+    query = """
+            SELECT f.film_id, f.title, f.description, f.release_year, c.name AS category_name,
+                   GROUP_CONCAT(CONCAT(a.first_name, ' ', a.last_name) SEPARATOR ', ') AS actors
+            FROM film f
+            JOIN film_category fc ON f.film_id = fc.film_id
+            JOIN category c ON fc.category_id = c.category_id
+            JOIN film_actor fa ON f.film_id = fa.film_id
+            JOIN actor a ON fa.actor_id = a.actor_id
+            WHERE f.film_id = %s
+            GROUP BY f.film_id;
+        """
+
+    cursor.execute(query, (id,))
+    results = cursor.fetchone()
+    cursor.close()
+    db.close()
+
+    return jsonify(results)
